4e417c7d5686a913b9b2cd2e4f867cfed1adf5e2
dlev- <daniel.leventhal+github@gmail.com>
2016-10-02 22:32:05 -0700

    added double header and consecutive game constraints

--- src/Constraints.ts
+++ src/Constraints.ts
@@ -1,12 +1,12 @@
-import GameSlotModel from "./ScheduleModel";
-import FieldModel from "./ScheduleModel";
+import GameSlotModel from "./GameSlotModel";
+import FieldModel from "./FieldModel";
 
 
 abstract class ScheduleConstraint
 {
 	private _cost: number;
 	
-	protected constructor(cost: number)
+	constructor(cost: number)
 	{
 		this._cost = cost;
 	}
@@ -19,45 +19,84 @@ abstract class ScheduleConstraint
 }
 
 class NeighboringFieldResolver {
-	private _fieldIdToNeighborsMap: any;
+	private _fieldIdToNeighborsMap: { [id: number]: { [fId: number]: boolean} };
 
 	constructor (fields: FieldModel[]) {
-
+//		this._fieldIdToNeighborsMap = fields;
 	}
 
-	public areNeighbors(fieldId1: number, fieldId2: number): boolean {
-		
+	public areNeighbors(fieldId1: number, fieldId2: number): boolean 
+	{
+		return fieldId2 in this._fieldIdToNeighborsMap[fieldId1];
 	}
 }
 
 
 class NonConsecutiveGameConstraint extends ScheduleConstraint {
-	private m_fieldNames: string[];
 	private static S_COST = 10000000.0;
-	
+	private static S_GAME_HOUR_DURATION = 2;
+	public static s_fieldNeighborResolver: NeighboringFieldResolver;
+
 	constructor ()
 	{	
 		super(NonConsecutiveGameConstraint.S_COST);
 	}
 	
-	public Pair<GameSlotInfo, Double> eval(ArrayList<GameSlotInfo> gameSlots) 
+	public eval(gameSlots: GameSlotModel[]) : GameSlotModel[]
 	{
-		int count = 0;
-		GameSlotInfo toRetGsi = null;
-		GameSlotInfo lastGsi = null;
-		for (GameSlotInfo gsi : gameSlots)
+		let badGameSlots: GameSlotModel[] = [];
+		for (let ii = 1; ii < gameSlots.length; ++ii)
 		{
-			if (lastGsi != null &&
-				lastGsi.getGameDay().getDateString().equals(gsi.getGameDay().getDateString()))
+			let lastGsm = gameSlots[ii-1];
+			let gsm = gameSlots[ii];
+			if (this.gamesOnSameDay(gsm, lastGsm))
 			{
-				if (!DoubleHeaderConstraint.areNeighboringFields(m_fieldNames, gsi, lastGsi) ||
-					Schedule.S_GAME_HOUR_DURATION != Math.abs(gsi.getGameTime().getHour() - lastGsi.getGameTime().getHour()))
+				let timeBetweenGames = Math.abs(gsm.time - lastGsm.time);
+				if (timeBetweenGames > NonConsecutiveGameConstraint.S_GAME_HOUR_DURATION ||
+					NonConsecutiveGameConstraint.s_fieldNeighborResolver.areNeighbors(gsm.fieldNum, lastGsm.fieldNum))
 				{
-					toRetGsi = gsi;
-					++count;
+					badGameSlots.push(gsm);
 				}
 			}
-			lastGsi = gsi;
 		}
-		return new Pair<GameSlotInfo, Double>(toRetGsi, count * getCost());
+		return badGameSlots;
 	}
+
+	private gamesOnSameDay(g1: GameSlotModel, g2: GameSlotModel): boolean 
+	{
+		return (g1.day == g2.day);
+	}
+}
+
+class NumGamesOnSameDayConstraint extends ScheduleConstraint {
+	private static S_COST = 100000.0;
+	private _maxGames: number;
+
+	constructor (maxGames: number)
+	{	
+		super(NumGamesOnSameDayConstraint.S_COST);
+		this._maxGames = maxGames;
+	}
+
+	public abstract eval(gameSlots: GameSlotModel[]) : GameSlotModel[] 
+	{
+		let badGameSlots: GameSlotModel[] = [];
+		let gamesOnDay = 1;
+		let prevDay = gameSlots[0].day;
+		for (let ii = 1; ii < gameSlots.length; ++ii) 
+		{
+			let gameDay = gameSlots[ii].day;
+			if (prevDay = gameDay) {
+				gamesOnDay ++;
+				if (gamesOnDay > this._maxGames) {
+					badGameSlots.push(gameSlots[ii]);
+				}
+			} else {
+				prevDay = gameDay;
+				gamesOnDay = 1;
+			}
+		}
+		return badGameSlots;
+	}
+}
+
